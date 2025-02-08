48830813bcc991bc7c2ff3c8675048032eeee861
jwson-automation <sonjungwoo9@gmail.com>
2025-02-08 22:12:54 +0900

    bugfix : widget_test ProviderScope漏れ修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_engineer_codecheck/main.dart';
 import 'package:flutter_engineer_codecheck/presenter/search_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_config/flutter_config.dart';
 
 void main() {
   setUpAll(() async {
@@ -14,7 +15,8 @@ void main() {
   group('GitHub Repository Search App Tests', () {
     testWidgets('First Screen Rendering Test', (tester) async {
       // MaterialAppでラップされたウィジェットをテストします
-      await tester.pumpWidget(const RepositorySearchApp());
+      await tester
+          .pumpWidget(const ProviderScope(child: RepositorySearchApp()));
       await tester.pumpAndSettle();
 
       // SearchScreenが表示されていることを確認します
